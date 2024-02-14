72385896b4315aa71272bc2f4579992ac4f7ffad
Vikas <iamvikasbareth@gmail.com>
2024-02-14 19:33:01 +0530

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
     </div>
   );
 }
